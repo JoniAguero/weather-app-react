a27378b67b9210719f91fcf5d604dea044f365cc
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-27 15:36:07 -0300

    Conectando API

--- wheather-app/src/components/WeatherLocation/index.js
+++ wheather-app/src/components/WeatherLocation/index.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Location from './Location';
 import WeatherData from './WeatherData';
 import './styles.css';
 
-import {
-    SUN,
-} from '../../constants/weathers';
+import { SUN } from '../../constants/weathers';
 
 const data = {
     temperature: 20,
@@ -14,11 +12,36 @@ const data = {
     wind: "10 m/s"
 }
 
-const WeatherLocation = () => (
-    <div className="weatherLocationCont">
-        <Location city={"Barcelona"}></Location>
-        <WeatherData data={data} ></WeatherData>
-    </div>
-);
+class WeatherLocation extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            city: 'Cordoba',
+            data: data
+        }
+    }
+
+    actualizarData = () => {
+        console.log("Actualizando datos");
+        fetch('http://api.openweathermap.org/data/2.5/weather?q=Cordoba,ar&APPID=92d440098068a10951f3cad240d5758a').then(res => {
+            return res.json()
+        }).then(info => {
+            console.log(info);
+        })
+        
+    }
+
+    render(){
+        return(
+            <div className="weatherLocationCont">
+                <Location city={this.state.city}></Location>
+                <WeatherData data={this.state.data} ></WeatherData>
+                <button onClick={this.actualizarData}>ACTUALIZAR</button>
+            </div>
+        )
+    }
+    
+};
 
 export default WeatherLocation;
